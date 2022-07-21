b06ee00aa92c9c869513a3a8de5c4bfb04b383b0
Timir <km.islam@asthait.com>
2022-07-21 12:17:03 +0600

    library pagination,query fixed

--- shared-component/PublicGridView.tsx
+++ shared-component/PublicGridView.tsx
@@ -9,44 +9,37 @@ import { useRouter } from 'next/router';
 const PublicGridView = (props: any) => {
   const runOneTime = useRef(true);
   const router = useRouter();
-  console.log(router.query);
 
-  const [librayActivities, setLibrayActivities] = useState<any>();
+  const [pageIndex, setPageIndex] = useState<any>(0);
 
-  const pageSize = 12;
-  const dummyData = librayActivities?.Items;
+  const pageDataSize = 12;
 
-  const [paginationState, setPaginationState] = useState({
-    data: dummyData,
-    totalPage: librayActivities?.Count || pageSize,
-    current: 1,
-    minIndex: 0,
-    maxIndex: 0,
+  const [paginationState, setPaginationState] = useState<any>({
+    data: null,
+    totalDataInPage: pageDataSize,
+    current: pageIndex,
   });
-  const [pageIndex, setPageIndex] = useState<any>();
+
   const onPageChange = (page: number) => {
     setPageIndex(page - 1);
-    // const response = api.LibraryActivity.allLibraryActivity(
-    //   page - 1,
-    //   12,
-    //   bodyObj
-    // );
-    // response
-    //   .then((response) => response.data)
-    //   .then((data) => {
-    //     console.log(data);
+    const response = api.LibraryActivity.allLibraryActivity(
+      page - 1,
+      12,
+      bodyObj
+    );
+    response
+      .then((response) => response.data)
+      .then((data) => {
+        setPaginationState((prevState: any) => ({
+          ...prevState,
+          data: data,
+          totalDataInPage: paginationState?.data?.Count / pageDataSize,
+          current: page,
+        }));
+      });
 
-    //     setLibrayActivities(data);
-    //   });
-    setPaginationState((prevState) => ({
-      ...prevState,
-      current: page,
-      minIndex: (page - 1) * pageSize,
-      maxIndex: page * pageSize,
-    }));
+    router.push({ pathname: '/library/all', query: { pageIndex: page - 1 } });
   };
-
-  console.log(pageIndex);
 
   const bodyObj = {
     activityType: null,
@@ -64,42 +57,34 @@ const PublicGridView = (props: any) => {
       response
         .then((response) => response.data)
         .then((data) => {
-          setLibrayActivities(data);
+          setPaginationState((prevState: any) => ({
+            ...prevState,
+            data: data,
+            totalDataInPage: paginationState?.data?.Count / pageDataSize,
+            current: pageIndex,
+            minIndex: 0,
+            maxIndex: pageDataSize,
+          }));
         });
     }
-    setPaginationState((prevState) => ({
-      ...prevState,
-      dummyData,
-      totalPage: librayActivities?.Count || pageSize,
-      minIndex: 0,
-      maxIndex: pageSize,
-    }));
   }, []);
 
   return (
     <div>
       <Row gutter={[16, 16]}>
-        {dummyData?.map(
-          (e: any, i: any) =>
-            i >= paginationState?.minIndex &&
-            i < paginationState?.maxIndex && (
-              <Link key={e.id} href={`/library/all/${e.id}`}>
-                <Col xs={24} xl={8} lg={12}>
-                  <CustomCard
-                    name={e.Title}
-                    id={e.id}
-                    members={e.MemberCount}
-                  />
-                </Col>
-              </Link>
-            )
-        )}
+        {paginationState?.data?.Items?.map((e: any, i: any) => (
+          <Link key={e.id} href={`/library/all/${e.id}`}>
+            <Col xs={24} xl={8} lg={12}>
+              <CustomCard name={e.Title} id={e.id} members={e.MemberCount} />
+            </Col>
+          </Link>
+        ))}
       </Row>
       <Pagination
         style={{ marginTop: 50 }}
-        pageSize={pageSize}
+        pageSize={pageDataSize}
         current={paginationState?.current}
-        total={librayActivities?.Count}
+        total={paginationState?.data?.Count}
         onChange={onPageChange}
       />
     </div>
